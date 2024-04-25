854a3ee80d5f6b39ffbebd8965a2b48a6149d542
izPanda <zachashaner@gmail.com>
2024-04-25 00:53:55 -0700

    Styling Changes
    Moved Carousel to the bottom and added a main section near the top with functional tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,3 +215,23 @@
 		});
 
 })(jQuery);
+
+
+
+function openTab(tabName) {
+    // Hide all tab contents
+    var tabContents = document.getElementsByClassName('tab-content');
+    for (var i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove('active');
+    }
+
+    // Remove active class from all tab buttons
+    var tabButtons = document.getElementsByClassName('tab-button');
+    for (var i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].classList.remove('active');
+    }
+
+    // Show the selected tab content and set the button as active
+    document.getElementById(tabName).classList.add('active');
+    event.currentTarget.classList.add('active');
+}
